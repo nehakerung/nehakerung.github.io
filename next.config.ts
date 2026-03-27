210b524d781ead67bf2a042ca3512dd2c60b528d
Neha Kerung <nehakerung@gmail.com>
2026-03-27 12:22:13 +0000

    Add github workflow for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repo = "YOUR_REPO_NAME"; // e.g. portfolio-website
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 export default nextConfig;
